1d6f1bcab647c548a96810d58f1a1bc036b0f55d
haohao <78696255@qq.com>
2018-06-22 18:39:47 +0800

    init project

---
+++ block.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+/*区块*/
+type Block struct {
+	Timestamp     int64  //区块的创建时间
+	Data          []byte //区块中有价值的数据
+	PrevBlockHash []byte //上一个区块的散列值
+	Hash          []byte //该区块的散列值
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+
+	return block
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(header)
+	b.Hash = hash[:]
+}
+
+/*生成 创世区块*/
+func NewGennesisBlock() *Block {
+	return NewBlock("Gennesis Block", []byte{})
+}
---
+++ blockchain.go
@@ -0,0 +1,25 @@
+package main
+
+/*区块链*/
+type Blockchain struct {
+	Blocks []*Block //区块数组(切片)
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	chainLen := len(bc.Blocks)
+	//找到上一个区块
+	prveBlock := bc.Blocks[chainLen-1]
+	//生成新区块
+	newBlock := NewBlock(data, prveBlock.Hash)
+	//将新区块加入区块链的区块数组中
+	bc.Blocks = append(bc.Blocks, newBlock)
+}
+
+func NewBlockchain() *Blockchain {
+	//创世区块
+	gennesisBlock := NewGennesisBlock()
+	//用创世区块生成区块链
+	blockchain := Blockchain{[]*Block{gennesisBlock}}
+
+	return &blockchain
+}
