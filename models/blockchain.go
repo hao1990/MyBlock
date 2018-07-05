c62e45f3cdd163fd4532fb768d05ca9a29bf6f52
UPVI-DH <upvi-dh@UPVI-DH.local>
2018-07-05 14:47:30 +0800

    注释

--- models/blockchain.go
+++ models/blockchain.go
@@ -9,6 +9,18 @@ import (
 const dbFile = "blockchain.db"
 const blocksBucket = "blocks"
 
+/*
+桶 bucket  > blocks
+
+数据库是价值对存储
+key:value
+1.
+    l:区块链尾端取款的 Hash
+2.
+	hash1 : 区块1
+	hash2 : 区块2
+
+*/
 /*区块链*/
 type Blockchain struct {
 	//Blocks []*Block //区块数组(切片)
@@ -40,12 +52,12 @@ func (bc *Blockchain) AddBlock(data string) {
 
 	newBlock := NewBlock(data, lastHash)
 	err = bc.DB.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		bucket := tx.Bucket([]byte(blocksBucket))
+		err := bucket.Put(newBlock.Hash, newBlock.Serialize())
 		if err != nil {
 			log.Panic(err)
 		}
-		err = b.Put([]byte("l"), newBlock.Hash)
+		err = bucket.Put([]byte("l"), newBlock.Hash)
 		if err != nil {
 			log.Panic(err)
 		}
@@ -80,6 +92,18 @@ func (bci *BlockchainInterator) Next() *Block {
 	return block
 }
 
+/*
+1.打开一个数据库文件。
+2.检查里面是否存了区块链。
+3.如果里面有：
+	1.创建一个新的Blockchain实例。
+	2.把Blockchain实例的末端设为数据库中存储的最后一个区块
+4.如果没有区块链：
+	1.创建创世区块
+	2.存入数据库
+	3.把创世区块的哈希存作为最后区块哈希存储
+	4.创建一个尾部指向创世区块的Blockchain实例
+*/
 func NewBlockchain() *Blockchain {
 	var tip []byte
 	db, err := bolt.Open(dbFile, 0600, nil)
@@ -87,9 +111,9 @@ func NewBlockchain() *Blockchain {
 		log.Panic(err)
 	}
 	err = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
+		bucket := tx.Bucket([]byte(blocksBucket))
 
-		if b == nil {
+		if bucket == nil {
 			fmt.Println("没有发现区块链 》》》 创建新的")
 			//创世区块
 			gennesisBlock := NewGennesisBlock()
@@ -112,7 +136,7 @@ func NewBlockchain() *Blockchain {
 			tip = gennesisBlock.Hash
 
 		} else {
-			tip = b.Get([]byte("l"))
+			tip = bucket.Get([]byte("l"))
 		}
 		return nil
 	})
